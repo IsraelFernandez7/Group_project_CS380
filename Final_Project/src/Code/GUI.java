2eabdd4e2aa03a8b76ade1673a8e38dcbd8d9caf
israfer7 <38960890+israfer7@users.noreply.github.com>
2023-11-02 22:13:45 -0700

    Added functions for the ticking timer and put it under one whole function

--- Final_Project/src/Code/GUI.java
+++ Final_Project/src/Code/GUI.java
@@ -2,7 +2,8 @@ package Code;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,7 +12,72 @@ public class GUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private int time = 0; 
+	private Timer timer = new Timer();
+	
+	
 
+	    public void start1() {
+	        timer.scheduleAtFixedRate(new TimerTask() {
+	            @Override
+	            public void run() {
+	                action();
+	            }
+	        }, 0, 1000);
+	    }
+
+	    public void stop1() {																//function to stop the timer
+	    	//stop timer
+	        timer.cancel();
+	    }
+
+	    public void reset1() {
+	    	//stop timer
+	    	timer.cancel();
+	        time = 0;
+	        //clear time
+	        lbl1.setText("00 : 00 : 00");													//label for the time 
+	    }
+
+	    public void action() {																//function to start the timer
+	        //increment by 1
+	    	time = time + 1;
+	    	//calling timer conversion
+	        int[] timing = timeConversion(time);
+	        //setting up the string for the timer
+	        String timeString = makeTimeString(timing[0], timing[1], timing[2]);
+	        lbl1.setText(timeString);														//label for the time
+	    }
+
+	    public int[] timeConversion(int seconds) {											//function to convet seconds to minutes then minutes to hours
+	        //three elements
+	    	int[] result = new int[3];
+	    	//seconds
+	        result[0] = seconds / 3600;
+	        //minutes
+	        result[1] = (seconds % 3600) / 60;
+	        //hours
+	        result[2] = (seconds % 3600) % 60;
+	        return result;
+	    }
+
+	    public String makeTimeString(int hours, int minutes, int seconds) {					//making the timer to the string
+	        String timeString = "";
+	        //hours
+	        timeString += String.format("%02d", hours);
+	        timeString += " : ";
+	        //minutes
+	        timeString += String.format("%02d", minutes);
+	        timeString += " : ";
+	        //seconds
+	        timeString += String.format("%02d", seconds);
+	        return timeString;
+	    }
+	
+
+	
+	
+	
 	/**
 	 * Launch the application.
 	 */
