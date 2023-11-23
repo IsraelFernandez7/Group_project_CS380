eaffc5ea02518d95b10ca367334c67ec7659c2ef
israfer7 <38960890+israfer7@users.noreply.github.com>
2023-11-22 17:48:20 -0800

    renamed the class to timerclass

--- Final_Project/src/Code/timerClass.java
+++ Final_Project/src/Code/timerClass.java
@@ -4,15 +4,17 @@ package Code;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class timer {
+public class timerClass {
 
 	private String timeString;
-	
 	public int time = 0; 
 	public Timer timer = new Timer();
 	
 	
-
+		/**
+		 * starting methot
+		 */
+	
 		public void start1() {
 	        timer.scheduleAtFixedRate(new TimerTask() {
 	            @Override
@@ -22,30 +24,43 @@ public class timer {
 	        }, 0, 1000);
 	    }
 
-	    public void stop1() {														//function to stop the timer
+		/**
+		 *function to stop the timer
+		 */
+	    public void stop1() {														
 	    	//stop timer
 	        timer.cancel();
 	    }
 
+	    /**
+	     * function to reset the timer
+	     */
 	    public void reset1() {
-	    	//stop timer
 	    	timer.cancel();
 	        time = 0;
 	        //clear time
-	        //lbl1.setText("00 : 00 : 00");											//label for the time 
+	        //lbl1.setText("00 : 00 : 00");			//****************************
 	    }
 
-	    public void action() {														//function to start the timer
+	    /**
+	     * //function to start the timer
+	     */
+	    public void action() {														
 	        //increment by 1
 	    	time = time + 1;
 	    	//calling timer conversion
 	        int[] timing = timeConversion(time);
 	        //setting up the string for the timer
 	        String timeString = makeTimeString(timing[0], timing[1], timing[2]);
-	        //lbl1.setText(timeString);												//label for the time
+	        //lbl1.setText(timeString);				//*****************************
 	    }
 
-	    public int[] timeConversion(int seconds) {									//function to convet seconds to minutes then minutes to hours
+	    /**
+	     * function to convert seconds to minutes then minutes to hours
+	     * @param seconds
+	     * @return
+	     */
+	    public int[] timeConversion(int seconds) {									
 	        //three elements
 	    	int[] result = new int[3];
 	    	//seconds
@@ -66,5 +81,5 @@ public class timer {
 	 
 	    public String getTimeString() {
 	    	return timeString;
-	}
+	    }
 }
