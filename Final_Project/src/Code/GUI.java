f4992b84919ddb942c0f20a0f5db219e60a5f961
israfer7 <38960890+israfer7@users.noreply.github.com>
2023-11-22 17:45:29 -0800

    I deleted the code for the timer since there was already a class included for it and instead created instances to use the contents in the timer class

--- Final_Project/src/Code/GUI.java
+++ Final_Project/src/Code/GUI.java
@@ -11,6 +11,7 @@ import javax.swing.border.EmptyBorder;
 
 public class GUI extends JFrame implements ActionListener {
 
+	private timerClass timerInstance;
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JFrame activityFrame;
@@ -34,6 +35,8 @@ public class GUI extends JFrame implements ActionListener {
 	private int time = 0; 
 	private Timer timer;
 	
+	
+	
 	public void recordsFrameOptions() {
 		
 	}
@@ -50,6 +53,7 @@ public class GUI extends JFrame implements ActionListener {
 		SetupFrame.add(mainPanel);
 		mainPanel.add(textpanePanel);
 		mainPanel.add(fieldPanel);
+		timerInstance = new timerClass();
 		mainPanel.add(buttonPanel);
 		fieldPanel.add(name);
 		buttonPanel.add(start);
@@ -80,9 +84,12 @@ public class GUI extends JFrame implements ActionListener {
 		buttonPanel.add(addNote);
 		buttonPanel.add(Reset);
 		activityPanel.setLayout(new BoxLayout(activityPanel, BoxLayout.Y_AXIS));
-		start1();
+        timerInstance = new timerClass();
+		timerInstance.start1();
 	}
 	
+	
+
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == startActivity) {
 			this.dispose();
@@ -93,69 +100,13 @@ public class GUI extends JFrame implements ActionListener {
 			activityFrameOptions();
 		}
 		if(e.getSource() == Pause) {
-			stop1();
+			timerInstance.stop1();
 		}
 		if(e.getSource() == Reset) {
-			reset1();
+			timerInstance.reset1();
 		}
 	}
-	public void start1() {
-	    	timer = new Timer();
-	        timer.scheduleAtFixedRate(new TimerTask() {
-	            @Override
-	            public void run() {
-	                action();
-	            }
-	        }, 0, 1000);
-	    }
-
-	 public void stop1() {																//function to stop the timer
-	    	//stop timer
-	        timer.cancel();
-	    }
-
-	    public void reset1() {
-	    	timer.cancel();
-	        time = 0;
-	        //clear time
-	        timerLabel.setText("00 : 00 : 00");													//label for the time 
-	        start1();
-	    }
-
-	    public void action() {																//function to start the timer
-	        //increment by 1
-	    	time = time + 1;
-	    	//calling timer conversion
-	        int[] timing = timeConversion(time);
-	        //setting up the string for the timer
-	        String timeString = makeTimeString(timing[0], timing[1], timing[2]);
-	        timerLabel.setText(timeString);														//label for the time
-	    }
-
-	    public int[] timeConversion(int seconds) {											//function to convet seconds to minutes then minutes to hours
-	        //three elements
-	    	int[] result = new int[3];
-	    	//seconds
-	        result[0] = seconds / 3600;
-	        //minutes
-	        result[1] = (seconds % 3600) / 60;
-	        //hours
-	        result[2] = (seconds % 3600) % 60;
-	        return result;
-	    }
-
-	    public String makeTimeString(int hours, int minutes, int seconds) {					//making the timer to the string
-	        String timeString = "";
-	        //hours
-	        timeString += String.format("%02d", hours);
-	        timeString += " : ";
-	        //minutes
-	        timeString += String.format("%02d", minutes);
-	        timeString += " : ";
-	        //seconds
-	        timeString += String.format("%02d", seconds);
-	        return timeString;
-	    }
+	
 	
 
 	
